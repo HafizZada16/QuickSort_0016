d66890cbad600f8384fbcd6472b9b8da6752d3d0
HafizZada16 <hafizzada16@gmail.com>
2024-03-28 16:36:59 +0700

    Add input while ( if , else )

--- QuickSort_0016/QuickSort_0016.cpp
+++ QuickSort_0016/QuickSort_0016.cpp
@@ -7,7 +7,16 @@ int mov_count = 0;
 int n;
 
 void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array : ";
+		cin >> n;
 
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
 }
 
 int main()
